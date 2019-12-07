7777712062ae1b8209926df2d7a166266ee1385a
MinghuiLi <liminghui1993@gmail.com>
2019-12-07 22:43:02 +1100

    update7 on angular app

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,7 +23,7 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = this.route.snapshot.paramMap.get('_id');
+    const id = this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
